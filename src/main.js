e3b078dab78c13b7c25397473a7d530b7fc5d4dc
Nick Ramsey <nramseysc@gmail.com>
2019-07-05 14:45:42 -0700

    set .gitignore and working on parsing API return

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
+
+$(document).ready(function() {
+  $('#doctors').click(function() {
+    let request = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
+    let response;
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+    const getElements = function(response) {
+      $('.results').text(`The doctors are ${response.meta}`);
+    };
+  });
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
     module.exports = {
       entry: './src/main.js',
@@ -16,6 +17,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
       plugins: [
           new UglifyJsPlugin({ sourceMap: true }),
           new CleanWebpackPlugin(['dist']),
+          new Dotenv(),
           new HtmlWebpackPlugin({
            title: 'Your Page Name',
            template: './src/index.html',
@@ -31,7 +33,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
                 'css-loader'
               ]
             },
-            
+
           ]
         }
       };
