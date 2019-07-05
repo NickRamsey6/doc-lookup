ac4fe128514823da375384818d2c443a0f2230f5
Nick Ramsey <nramseysc@gmail.com>
2019-07-05 16:11:09 -0700

    add functionality for querying via symptoms

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ import 'bootstrap';
 $(document).ready(function() {
   $('#doctors').click(function() {
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=full-name-asc&skip=0&limit=1&user_key=2ac8627f15744eae8b7df8480f224360`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=full-name-asc&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
@@ -35,9 +35,13 @@ $(document).ready(function() {
     };
     request.open("GET", url, true);
     request.send();
+    // let docs = [];
     const getElements = function(response) {
-      $('.results').text(`The doctors are ${response.data["0"].profile.first_name}`);
-      console.log(response.bikes);
+      // for(let i = 0; i < docs.length, i++) {
+        $('.results').text(`The doctors are ${response.data.values().profile}`);
       }
+      // $('.results').text(`The doctors are ${response.data[i].profile.first_name}`);
+      // console.log(response.bikes);
+
   });
 });
