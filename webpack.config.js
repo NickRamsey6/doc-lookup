3d510d6d921a685045544da1a9099368253bf439
Nick Ramsey <nramseysc@gmail.com>
2019-07-05 11:38:24 -0700

    additional webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-};  
+    module.exports = {
+      entry: './src/main.js',
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+      },
+      plugins: [
+          new CleanWebpackPlugin(['dist']),
+          new HtmlWebpackPlugin({
+           title: 'Your Page Name',
+           template: './src/index.html',
+           inject: 'body'
+           })
+         ],
+      module: {
+          rules: [
+            {
+              test: /\.css$/,
+              use: [
+                'style-loader',
+                'css-loader'
+              ]
+            }
+          ]
+        }
+      };    
