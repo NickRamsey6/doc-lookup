c967f7eed506928b584e4f7b95cbe74167794c80
Nick Ramsey <nramseysc@gmail.com>
2019-07-05 15:03:36 -0700

    re add lost files

--- src/main.js
+++ src/main.js
@@ -2,22 +2,41 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 
+// $(document).ready(function() {
+//   $('#doctors').click(function() {
+//     let request = new XMLHttpRequest();
+//     const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
+//     let response;
+//     request.onreadystatechange = function() {
+//       if (this.readyState === 4 && this.status === 200) {
+//         const response = JSON.parse(this.responseText);
+//         getElements(response);
+//
+//       }
+//     };
+//     request.open("GET", url, true);
+//     request.send();
+//     const getElements = function(response) {
+//       $('.results').text(`The doctors are ${response.data['0']}`);
+//     };
+//   });
+// })
+
+
 $(document).ready(function() {
   $('#doctors').click(function() {
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
-    let response;
+    const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=%E2%80%9CPortland%2C%20OR%E2%80%9D&distance=10&stolenness=proximity&access_token=api_key`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
-
       }
     };
     request.open("GET", url, true);
     request.send();
     const getElements = function(response) {
-      $('.results').text(`The doctors are ${response.meta}`);
-    };
+      $('.results').text(`The doctors are ${response.bikes['0']}`);
+      }
   });
-})
+});
