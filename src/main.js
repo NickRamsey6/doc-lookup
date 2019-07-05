0fe35104b3afd1d89a4a4780a536789cd1073411
Nick Ramsey <nramseysc@gmail.com>
2019-07-05 16:39:28 -0700

    looping functionality for quicker results

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ import 'bootstrap';
 $(document).ready(function() {
   $('#doctors').click(function() {
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=full-name-asc&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&user_location=37.773%2C-122.413&sort=full-name-asc&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
@@ -37,11 +37,12 @@ $(document).ready(function() {
     request.send();
     // let docs = [];
     const getElements = function(response) {
-      // for(let i = 0; i < docs.length, i++) {
-        $('.results').text(`The doctors are ${response.data.values().profile}`);
+      for(let i = 0; i < response.data.length; i++) {
+        console.log(response.data.length);
+        $('.results').text(`The doctors are ${response.data[i].profile.first_name}`);
       }
       // $('.results').text(`The doctors are ${response.data[i].profile.first_name}`);
       // console.log(response.bikes);
-
+    }
   });
 });
