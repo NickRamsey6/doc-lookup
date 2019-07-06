5ed0fd73dda5e9d5bb404a3a00c10887aea65df5
Nick Ramsey <nramseysc@gmail.com>
2019-07-06 15:06:01 -0700

    adapt api query to account for issues and names

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import 'bootstrap';
 $(document).ready(function() {
   $('#location').click(function() {
     const city = $('#city').val();
-    console.log(city);
+    const name = $('#name').val();
+    $('#name').val("");
+    const issue = $('#issue').val();
+    $('#issue').val("");
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=2ac8627f15744eae8b7df8480f224360`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
