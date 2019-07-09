cd09bf09a1f1e841e8661c9a4975977164ea0e75
Nick Ramsey <nramseysc@gmail.com>
2019-07-08 21:23:51 -0700

    seperated ui and business logic

--- src/docs.js
+++ src/docs.js
@@ -1,17 +1,17 @@
-// export class DoctorFinder {
-//   findDoctors() {
-//     return new Promise(function(resolve, reject) {
-//           let request = new XMLHttpRequest();
-//           let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`;
-//           request.onload = function () {
-//             if (this.status === 200) {
-//               resolve(request.responseText);
-//             } else {
-//               reject(Error(request.statusText));
-//             }
-//           }
-//           request.open("GET", url, true);
-//           request.send();
-//     });
-//   }
-// }
+export class DoctorFinder {
+  findDoctors(city, name, issue) {
+    return new Promise(function(resolve, reject) {
+          let request = new XMLHttpRequest();
+          let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+          request.onload = function () {
+            if (this.status === 200) {
+              resolve(request.responseText);
+            } else {
+              reject(Error(request.statusText));
+            }
+          }
+          request.open("GET", url, true);
+          request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
+import {DoctorFinder} from './docs.js';
 
 
 
@@ -11,32 +12,36 @@ $(document).ready(function() {
     $('#name').val("");
     const issue = $('#issue').val();
     $('#issue').val("");
-    let request = new XMLHttpRequest();
+  //   let request = new XMLHttpRequest();
   // const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-  const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+  // const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`;
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
+    // request.onreadystatechange = function() {
+    //   if (this.readyState === 4 && this.status === 200) {
+    //     const response = JSON.parse(this.responseText);
+    //     getElements(response);
+    //   }
+    // }
+    // request.open("GET", url, true);
+    // request.send();
 
-    const getElements = function(response) {
-      for (let i = 0; i < response.data.length; i++) {
+    let doctorFinder = new DoctorFinder();
+    let promise = doctorFinder.findDoctors(city, name, issue);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      for (let i = 0; i < body.data.length; i++) {
         // return doctor name
-        $('.results').append('<br>').append('<ui>' + `Dr. ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}`);
+        $('.results').append('<br>').append('<ui>' + `Dr. ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`);
         //return doctor address
-        $('.results').append('<br>').append('<li>' + `Address: ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip}`);
+        $('.results').append('<br>').append('<li>' + `Address: ${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}`);
         //return doctor phone number
-        $('.results').append('<li>' + `Phone: ${response.data[i].practices[0].phones[0].number}`);
+        $('.results').append('<li>' + `Phone: ${body.data[i].practices[0].phones[0].number}`);
         //return doctor website url
-        $('.results').append('<li>' + `Website: ${response.data[i].practices[0].website}`);
+        $('.results').append('<li>' + `Website: ${body.data[i].practices[0].website}`);
         //return doctor new patients
-        $('.results').append('<li>' + `Accepting New Patients: ${response.data[i].practices[0].accepts_new_patients}`);
+        $('.results').append('<li>' + `Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}`);
       }
-    }
+    })
   });
 });
