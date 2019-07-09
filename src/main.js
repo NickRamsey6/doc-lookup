90642d8f5ccde22063a593aac174a720c62650e8
Nick Ramsey <nramseysc@gmail.com>
2019-07-08 21:53:24 -0700

    error message for bad api call

--- src/main.js
+++ src/main.js
@@ -12,18 +12,7 @@ $(document).ready(function() {
     $('#name').val("");
     const issue = $('#issue').val();
     $('#issue').val("");
-  //   let request = new XMLHttpRequest();
-  // const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-  // const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`;
 
-    // request.onreadystatechange = function() {
-    //   if (this.readyState === 4 && this.status === 200) {
-    //     const response = JSON.parse(this.responseText);
-    //     getElements(response);
-    //   }
-    // }
-    // request.open("GET", url, true);
-    // request.send();
 
     let doctorFinder = new DoctorFinder();
     let promise = doctorFinder.findDoctors(city, name, issue);
@@ -42,6 +31,9 @@ $(document).ready(function() {
         //return doctor new patients
         $('.results').append('<li>' + `Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}`);
       }
-    })
+    }, function(error) {
+        $('.errors').text(`There was an error processing your request: ${error.message}`);
+    });
   });
+
 });
