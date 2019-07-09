d651a359cca279dd2b92c80acf3db46abc6373d5
Nick Ramsey <nramseysc@gmail.com>
2019-07-08 22:00:28 -0700

    a test api query to trigger an error message

--- src/docs.js
+++ src/docs.js
@@ -3,6 +3,8 @@ export class DoctorFinder {
     return new Promise(function(resolve, reject) {
           let request = new XMLHttpRequest();
           let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=${city}&user_location=37.773%2C-122.413&sort=distance-asc&fields=data%2Cpractices%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+          //Bad API request for testing error message (returns 400, triggering error text)
+          // let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&limit=a&user_key=${process.env.API_KEY}`;
           request.onload = function () {
             if (this.status === 200) {
               resolve(request.responseText);
